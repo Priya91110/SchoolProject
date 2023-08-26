7df489098382f0b0d3f56a8799939b6c9b6dbc64
Priyanka <priyankacarpenter30@gmail.com>
2023-08-26 16:40:42 +0530

    Added Student subject class

--- student_info.py
+++ student_info.py
@@ -2,3 +2,26 @@ class Student:
     def identity(self):
         print("name ")
         
+print("Thislkasjdlk;asjd ")
+
+class History:
+    pass
+
+hist = History()
+
+class Subject:
+    hist = History()
+    def __init__(self) -> None:
+        pass
+
+
+class Science:
+    def __init__(self) -> None:
+        pass
+    
+sci = Science()
+
+
+class Maths:
+    def __init__(self) -> None:
+        pass
